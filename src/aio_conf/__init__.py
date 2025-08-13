826876fe3a6282e30fca4c2eea4591e3aa4d8935
Taylor B. <t.blackstone@inspyre.tech>
2025-08-12 19:24:42 -0500

    Add dynamic versioning support and update project metadata

--- src/aio_conf/__init__.py
+++ src/aio_conf/__init__.py
@@ -1,3 +1,10 @@
 from .core import OptionSpec, ConfigSpec, AIOConfig
 
+
+try:
+    from .meta._version import __version__  # type: ignore  # noqa
+except ImportError:
+    __version__ = "0.0.0+local"
+
+
 __all__ = ["OptionSpec", "ConfigSpec", "AIOConfig"]
---
+++ src/aio_conf/meta/__init__.py
@@ -0,0 +1,17 @@
+"""
+
+
+Author: 
+    Inspyre Softworks
+
+Project:
+    AIO-Conf
+
+File: 
+    src/aio_conf/meta/__init__.py
+ 
+
+Description:
+    
+
+"""
